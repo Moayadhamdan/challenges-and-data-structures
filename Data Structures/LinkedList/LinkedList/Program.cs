8d26a9a320f39962032fe39c338ab448321350f7
Moayadhamdan <hamadanjo@gmail.com>
2024-07-12 04:13:07 +0300

    Linked List Implementation

---
+++ Data Structures/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListClass list = new LinkedListClass();
+
+            // Adding nodes to the list
+            list.Head = new Node(2);
+            list.Head.Next = new Node(4);
+            list.Head.Next.Next = new Node(8);
+            list.Head.Next.Next.Next = new Node(16);
+
+            // Test PrintList method  
+            Console.WriteLine("List:");
+            list.PrintList();
+
+
+            // Test Includes method
+            Console.WriteLine("\nTesting Includes method:");
+            Console.WriteLine($"Does the list include 4? {list.Includes(4)}");
+            Console.WriteLine($"Does the list include 15? {list.Includes(15)}");
+
+            // Test Remove method
+            Console.WriteLine("\nRemoving node with data 4:");
+            list.Remove(4);
+            list.PrintList();
+
+        }
+    }
+}
